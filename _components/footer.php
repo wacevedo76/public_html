9bac8ece398cfbfafbb30034d18cada4d8b7675c
William Acevedo <william.acevedo@gmail.com>
2020-03-03 11:02:05 +0100

    created footer file (frame)

---
+++ _components/footer.php
@@ -0,0 +1,28 @@
+<footer>
+  <div class="row">
+    <div id="footer-contact" class="col">
+      <p>Biuro Stockholm</p>
+      <p>Upplagsvagen 1, 4 tr,<br>
+      117 43 Stockholm</p>
+
+      <p>+ 46 (0)76 - 665 66 67<br>
+      kontor@kam.nu</p>
+
+      <p> <img src="../_resources/fb-icon.png" alt="">
+          <img src="../_resources/li-icon.png" alt=""> </p>
+
+    </div>
+    <div id="footer-nav" class="col">
+      <div class="footer-nav-list">
+
+      </div>
+      <div class="footer-nav-list">
+
+      </div>
+      <div class="footer-nav-list">
+
+      </div>
+    </div>
+
+  </div>
+</footer>
