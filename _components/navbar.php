b5b7912ab152e1859c429535db41357c4b2ae8d4
William Acevedo <william.acevedo@gmail.com>
2020-03-03 10:59:33 +0100

    added new links to navbar

--- _components/navbar.php
+++ _components/navbar.php
@@ -5,9 +5,14 @@
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-item nav-link active" href="#"><img src="../_resources/logo.png" alt=""><span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      <a class="nav-item nav-link" href="#">Uslugi</a>
+      <a class="nav-item nav-link" href="#">O firmie</a>
+      <a class="nav-item nav-link" href="#">Referencje</a>
+      <a class="nav-item nav-link" href="#">Wyroznienia</a>
+      <a class="nav-item nav-link" href="#">Wydarzenia</a>
+      <a class="nav-item nav-link" href="#">Kontakt</a>
+      <a class="nav-item nav-link" href="#">PL</a>
+      <a class="nav-item nav-link" href="#">Zapytaj</a>
     </div>
   </div>
 </nav>
